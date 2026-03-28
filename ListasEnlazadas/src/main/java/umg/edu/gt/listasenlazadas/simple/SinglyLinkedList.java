28f6e5df9061f7fdda9e713cc81075ad3b82c134
Tania Andrea <tmirandar@miumg.edu.gt>
2026-03-27 19:58:56 -0600

    Realizacion de Reto 1

--- ListasEnlazadas/src/main/java/umg/edu/gt/listasenlazadas/simple/SinglyLinkedList.java
+++ ListasEnlazadas/src/main/java/umg/edu/gt/listasenlazadas/simple/SinglyLinkedList.java
@@ -104,10 +104,19 @@ public class SinglyLinkedList<T> {
      * @param value valor a buscar
      * @return cantidad de ocurrencias encontradas
      */
+    
     public int countOccurrences(T value) {
-        throw new UnsupportedOperationException(
-                "TODO RETO: Implementar countOccurrences(T value) "
-                + "en SinglyLinkedList.");
+    	int count = 0;
+        SimpleNode<T> current = head;
+        
+        while (current != null) {
+            if (isSameValue(current.getValue(), value)) {
+                count++;
+            }
+            current = current.getNext();
+        }
+        
+        return count;
     }
 
     @Override
